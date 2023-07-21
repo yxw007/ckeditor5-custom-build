72cfbc93b13297712444faa005c8f94632074419
Potter <75838259@qq.com>
2023-07-21 10:13:18 +0800

    chore: update dependency

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,7 +30,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-// import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 class Editor extends ClassicEditor { }
 
@@ -63,7 +63,7 @@ Editor.builtinPlugins = [
   Table,
   TableToolbar,
   TextTransformation,
-  // SimpleUploadAdapter
+  SimpleUploadAdapter
 ];
 
 // Editor configuration.
